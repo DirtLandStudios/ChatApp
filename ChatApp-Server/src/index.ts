7424250339caf12943817484aab5951079ce31a3
bananaboy139 <matteob319@gmail.com>
2022-03-23 19:14:43 +0100

    fixed message receiving and redistrobuting

--- ChatApp-Server/src/index.ts
+++ ChatApp-Server/src/index.ts
@@ -1,23 +1,23 @@
-const express = require('express')
+//const express = require('express')
 const ws = require('ws')
 const https = require('https')
 
 type ChatMessage = {message: string, user: string}
 // Set up a headless websocket server that prints any
 // events that come in.
-var ChatPort = 8999;
+const ChatPort = 8999;
 const wss = new ws.WebSocketServer({ port: ChatPort })
 var Users: string[]
 var Chat: ChatMessage[]
 
-wss.on('connection', function connection(ws, request, client) {
+wss.on('connection', (ws: any /*Need type!*/) => {
 
-	ws.on('message', function message(data: string) {
+	ws.on('message', (data: string) => {
     	console.log('received: %s', data)
-		
-		Chat.push( {data, client} )
-		wss.clients.ForEach(function each(client) {
-			client.send()
+		var message: ChatMessage = JSON.parse(data)
+		Chat.push( message )
+		wss.clients.ForEach((client: any /*Need type!*/) => {
+			client.send(message)
 		})
 	});
   
