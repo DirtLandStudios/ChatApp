399f65d0071e6262d09d45b88af0fe2d6fc7f4f0
bananaboy319 <matteob319@gmail.com>
2022-03-24 12:52:47 +0100

    cleanup and bug fix MAJOR BUG:     //Object is possibly 'null'.     //Property 'value' does not exist on type 'HTMLElement'. var _message: string = document.getElementById("textbox" /*WHAT IS ID OF tEXT BOX??*/).value || ""

--- ChatApp-Client/src/main.ts
+++ ChatApp-Client/src/main.ts
@@ -1,22 +1,31 @@
 // Create WebSocket connection.
-var Ip: string = "";
-var Port: string = "";
-var UserName: string;
-const socket = new WebSocket(`wss://${Ip}:${Port}`, "ChatApp")
+var Ip: string = "localhost";
+var Port: string = "8999";
+var UserName: string = "";
 type ChatMessage = {message: string, user: string}
+const socket = new WebSocket(`wss://${Ip}:${Port}`, "ChatApp")
+
 // Connection opened
-socket.addEventListener('open', function (event) {
+socket.onopen = (event) => {
     socket.send(UserName)
-});
+};
 
-function get_message(message: Event) {
-}
-
-function send_message(_message: string) {
+function send_message() {
+    //Object is possibly 'null'.
+    //Property 'value' does not exist on type 'HTMLElement'.
+    var _message: string = document.getElementById("textbox" /*WHAT IS ID OF tEXT BOX??*/).value || ""
     var Chatmessage: ChatMessage = {message: _message, user: UserName}
     socket.send(JSON.stringify(Chatmessage))
 }
 
 // Listen for messages
-socket.addEventListener('message', get_message)
+socket.onmessage = (_message: MessageEvent) => {
+    var incoming: ChatMessage = JSON.parse(_message.data)
+    var message: string = `${incoming.user}: ${incoming.user}`
+    //add this to chat history
+    //document.getElementById
+}
 //message form: {message, user}
+function End() {
+    socket.close()
+}
