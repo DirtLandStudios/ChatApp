c4a4e724d9548a10cca41236c44d36b1b91a32c2
bananaboy139 <matteob319@gmail.com>
2022-03-30 22:09:20 +0200

    try to host client on server NOTHING WORKS

---
+++ ChatApp-Server/src/client.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express"
+const express = require('express')
+const fs = require("fs")
+var https = require('https')
+
+import {KeyFile, CertFile} from "./Files"
+var privateKey  = fs.readFileSync(KeyFile)
+var certificate = fs.readFileSync(CertFile)
+var credentials = {key: privateKey, cert: certificate}
+const app = express()
+// GET method route
+const port = 8000 
+
+app.all('/ChatApp', (req: Request, res: Response, next: NextFunction) => {
+	res.send("Connected")
+	//res.render("/ChatApp/Client/page.html")
+	next() // pass control to the next handler
+})
+
+
+var httpsServer = https.createServer(credentials, app)
+httpsServer.listen(port)
